9d90823e45db1611796b951ec7627100c7c675da
ouazzaniredouane <r.ouazzani.chahdi1986@gmail.com>
2025-01-20 11:46:06 +0100

    adding ai chat

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import '@aws-amplify/ui-react/styles.css'; 
-import ConfigureAmplifyClientSide from '@/components/ConfigureAmplifyClientSide';
+import { ConfigureAmplify } from "@/components/ConfigureAmplify";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ConfigureAmplifyClientSide />
+        <ConfigureAmplify />
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,20 @@
-import { revalidatePath } from "next/cache";
-import { AuthGetCurrentUserServer, cookiesClient } from "@/utils/amplify-utils";
-import Logout from "@/components/Logout";
-import RecipeGenerator from "@/components/RecipeGenerator";
-export default async function Home() {
-  const user = await AuthGetCurrentUserServer();
-  const {data: todos} = await cookiesClient.models.Todo.list()
+"use client";
 
-  async function addTodo(data: FormData) {
-    "use server";
-    const title = data.get("title") as string;
-    await cookiesClient.models.Todo.create({
-      content: title,
-      done: false,
-      priority: "medium",
-    });
-    revalidatePath("/");
-  }
+import { AIConversation, createAIHooks } from "@aws-amplify/ui-react-ai"; 
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
 
+const client = generateClient<Schema>();
+const {useAIConversation} = createAIHooks(client);
+export default function Page() {
+
+  const [{data: {messages}}, sendMessage] = useAIConversation("chat");
   return (
     <>
-      <h1>Hello, Amplify 👋</h1>
-      {user && <Logout />}
-      <form action={addTodo}>
-        <input type="text" name="title" />
-        <button type="submit">Add Todo</button>
-      </form>
-
-      <ul>
-        {todos && todos.map((todo) => <li key={todo.id}>{todo.content}</li>)}
-      </ul>
-      <RecipeGenerator />
+    <AIConversation 
+    messages={messages} 
+    handleSendMessage={sendMessage}/>
     </>
-  );
+  )
+  
 }
